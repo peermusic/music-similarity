2c157a87812eb5f930757c2f7912d4046e750cfd
queicherius <queicherius@gmx.de>
2015-12-15 00:36:39 +0100

    Request music similarity information (peermusic/user-stories#24)

--- README.md
+++ README.md
@@ -4,3 +4,28 @@ This module provides similarity information for music tracks. It takes music
 metadata as an argument, and requests results for similar music tracks from a 
 [scraping server](https://github.com/peermusic/node-scraping-server). The module broadens
 the search to provide a certain amount of results.
+
+## Install
+
+```sh
+npm install https://github.com/peermusic/music-similarity
+```
+
+```js
+var similarity = require('music-similarity')
+```
+
+For reference see the [Browserify Handbook](https://github.com/substack/browserify-handbook#how-node_modules-works).
+
+## Usage
+
+```js
+var similarity = require('music-similarity')
+
+// Get similarity using a list of scraping servers and a metadata object 
+var servers = ['http://test.de/scraping']
+var meta = {title: '...', artist: '...', album: '...', genre: '...'}
+similarity(servers, meta, function (similar_tracks) {
+  // similar_tracks is an array of metadata objects
+})
+```
---
+++ index.js
@@ -0,0 +1,126 @@
+var storage = new (require('in-memory-storage'))()
+var rusha = new (require('rusha'))()
+var xhr = require('xhr')
+var async = require('async')
+
+module.exports = MusicSimilarity
+
+// Provide similarity information for music files
+function MusicSimilarity (servers, metadata, callback) {
+  // Check the cache if we already have the results we want
+  var hash = rusha.digestFromString(JSON.stringify(metadata))
+  var cache = storage.get(hash)
+
+  if (cache) {
+    callback(cache)
+    return
+  }
+
+  // Build the possible queries and send them to the server
+  // until we have a certain amount of results
+  var queries = possibleQueries(metadata)
+  getSimilarTracks(servers, queries, function (tracks) {
+    storage.set(hash, tracks)
+    callback(tracks)
+  })
+}
+
+// Build the array of possible metadata requests
+function possibleQueries (metadata) {
+  var queries = []
+  if (metadata.title) { queries.push(_only(metadata, ['title', 'album', 'artist'])) }
+  if (metadata.album) { queries.push(_only(metadata, ['album', 'artist'])) }
+  if (metadata.artist) { queries.push(_only(metadata, ['artist'])) }
+  if (metadata.genre) { queries.push(_only(metadata, ['genre'])) }
+  return queries
+}
+
+// Get only the set keys of an object (but not if they are not set)
+function _only (object, keys) {
+  var clean = {}
+  for (var i = 0; i !== keys.length; i++) {
+    if (!object[keys[i]]) {
+      return
+    }
+    clean[keys[i]] = object[keys[i]]
+  }
+  return clean
+}
+
+// Recursively request similarity information with more and more broad queries
+function getSimilarTracks (servers, queries, callback, i, results) {
+  i = i || 0
+  results = results || []
+  var query = queries[i]
+  var max_results = 1
+
+  requestSimilar(servers, query, function (request_results) {
+    // Add the results to our results
+    results = results.concat(request_results)
+    results = _unique(results)
+
+    // Request until either we have no queries at the
+    // server anymore or we reached the maximum results
+    if (!queries[i + 1] || results.length >= max_results) {
+      callback(results)
+      return
+    }
+
+    getSimilarTracks(servers, queries, callback, i + 1, results)
+  })
+}
+
+// Only get unique results (this is a bit hacky, but less overhead than deep comparison)
+function _unique (results) {
+  results = results.map(function (x) { return JSON.stringify(x) })
+  results = results.filter(function (value, index, self) { return self.indexOf(value) === index })
+  results = results.map(function (x) { return JSON.parse(x) })
+  return results
+}
+
+// Go through each server and request information for this piece of metadata
+function requestSimilar (servers, metadata, callback) {
+  async.map(servers, function (server, c) {
+    requestFromServer(server, metadata, function (response) { c(null, response) })
+  }, function (err, results) {
+    if (err) throw err
+    results = results.reduce(function (x, y) { return x.concat(y) })
+    results = _unique(results)
+    callback(results)
+  })
+}
+
+// Request metadata from a single server
+function requestFromServer (server, metadata, callback) {
+  callback([{title: 'Honey, Honey', artist: 'ABBA', album: 'The Albums', genre: 'Pop'}])
+  return
+
+  // FIXME This code is theoretical until the scrapping server is done - but it should work! :)
+
+  var request_object = {
+    url: server,
+    method: 'POST',
+    json: {payload: metadata},
+    headers: {'Content-Type': 'application/json'}
+  }
+
+  xhr(request_object, function (error, response, body) {
+    // Error handling for bad server requests
+    if (error || response.statusCode !== 200) {
+      console.error('Error requesting metadata from scraping server (' + server + ')', metadata, error, response)
+      callback([])
+      return
+    }
+
+    // Error handling for bad server responses
+    try {
+      body = JSON.parse(body)
+    } catch (e) {
+      console.error('Error parsing metadata from scraping server (' + server + ')', metadata)
+      callback([])
+      return false
+    }
+
+    callback(body)
+  })
+}
